718bb482c506815f0da2caec2f322a282186dac9
afern <afern@LAPTOP-9NLR21M1>
2023-10-23 20:20:20 -0600

    Add week 6

---
+++ app/week6/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import { useState} from "react"
+import Home from '../home'
+import StudentInfo from '../studentinfo'
+import ItemList from './item-list'
+import NewItem from './new-item'
+import itemsData from "./items"
+
+export default function Main() {
+	const [items, setItems] = useState(itemsData);
+	const handleAddItem = (item) => {setItems((items) => [...items, item]);};
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<section className="max-w-5xl w-full items-center justify-between lg:flex">
+				<h1 className="text-4xl">My Shopping List</h1><br/>
+				<StudentInfo/>
+			</section>
+			<section className="max-w-5xl w-full items-start space-y-5 justify-between lg:flex">
+				<ItemList items={items}/>
+				<NewItem onAddItem={handleAddItem}/>
+			</section>
+			<section className="max-w-5xl sticky bottom-10 items-center justify-between lg:flex">
+				<Home/>
+			</section>
+		</main>
+	)
+}
